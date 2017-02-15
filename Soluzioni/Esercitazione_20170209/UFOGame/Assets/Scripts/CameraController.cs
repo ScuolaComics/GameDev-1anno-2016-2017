821aff000b4e02385049d73188365b75dc11eb07
Leonardo Passeri <passeri.leonardo@gmail.com>
2017-02-15 19:53:03 +0100

    Commit progetto UFOGame

---
+++ Soluzioni/Esercitazione_20170209/UFOGame/Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour 
+{
+	public Transform playerTransform;
+	private Vector3 _offset;
+
+	// Use this for initialization
+	void Start () 
+	{
+		_offset = transform.position - playerTransform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () 
+	{
+		transform.position = playerTransform.position + _offset;
+	}
+}
---
+++ Soluzioni/Esercitazione_20170209/UFOGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+	public float acceleration = 10.0f;
+	public Text countText;
+	public Text winText;
+
+	private Rigidbody2D _rb2D;
+	private int _count;
+
+	// Use this for initialization
+	void Start () 
+	{
+		_rb2D = this.gameObject.GetComponent<Rigidbody2D>();
+		_count = 0;
+		Debug.Assert(countText != null);
+		countText.text = "Count: " + _count.ToString();
+		Debug.Assert(winText != null);
+		winText.gameObject.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate ()
+	{
+		float moveVertical = Input.GetAxis("Vertical");
+		float moveHorizontal = Input.GetAxis("Horizontal");
+
+		Vector2 force = new Vector2(moveHorizontal, moveVertical);
+		force *= acceleration;
+		_rb2D.AddForce(force);
+	}
+
+	void OnTriggerEnter2D(Collider2D other) 
+	{
+		if (other.CompareTag("PickUp"))
+		{
+			other.gameObject.SetActive(false);
+			_count++;
+			Debug.Assert(countText != null);
+			countText.text = "Count: " + _count.ToString();
+
+			if (_count >= 12)
+			{
+				Debug.Assert(winText != null);
+				winText.gameObject.SetActive(true);
+			}
+		}
+	}
+}
