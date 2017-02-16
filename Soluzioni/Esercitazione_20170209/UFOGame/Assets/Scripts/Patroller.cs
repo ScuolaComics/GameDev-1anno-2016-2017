290392b739758f4a7f3359f964a5f5c31e34ef47
Leonardo Passeri <passeri.leonardo@gmail.com>
2017-02-16 12:17:00 +0100

    Realizzato pattugliamento lineare dei nemici.

---
+++ Soluzioni/Esercitazione_20170209/UFOGame/Assets/Scripts/Patroller.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patroller : MonoBehaviour 
+{
+	public Transform _patrolTranform;
+	public GameObject _target;
+
+	public float _speed = 10.0f;
+
+	private Vector3 _initialPosition;
+	private Vector3 _destination;
+
+	private float _startTime;
+	private float _journeyLength;
+
+	// Use this for initialization
+	void Start ()
+	{
+		_initialPosition = _patrolTranform.position;
+		_destination = _target.transform.position;
+		_startTime = Time.time;
+		_journeyLength = Vector3.Distance(_destination, _initialPosition);
+
+		_target.SetActive(false);
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float distanceCovered = (Time.time - _startTime) * _speed;
+		float fractionJourney = distanceCovered / _journeyLength;
+
+		_patrolTranform.position = Vector3.Lerp(_initialPosition, _destination, fractionJourney);
+
+		if (_patrolTranform.position == _destination)
+		{
+			Vector3 tempPosition = _initialPosition;
+			_initialPosition = _destination;
+			_destination = tempPosition;
+
+			_startTime = Time.time;
+		}
+	}
+}
