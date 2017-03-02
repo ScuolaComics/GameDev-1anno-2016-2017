3958ad558c00cfab03f1b71304eabd4fc3e17770
unknown <l.passeri@DOCENTE.comics.local>
2017-03-02 13:05:09 +0100

    Commit progetto Survival Shooter (lezione 2)

---
+++ Unity/SurvivalShooter/Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour 
+{
+	public Transform playerTransform;
+	public float smoothing = 5f;
+
+	private Vector3 _offset;
+
+	// Use this for initialization
+	void Start () 
+	{
+		if (playerTransform != null) 
+		{
+			_offset = transform.position - playerTransform.position;
+		}
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		if (playerTransform != null) 
+		{
+			Vector3 newCameraPosition = playerTransform.position + _offset;
+			transform.position = Vector3.Lerp (transform.position, newCameraPosition, smoothing * Time.deltaTime);
+		}
+	}
+}
---
+++ Unity/SurvivalShooter/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+[RequireComponent (typeof(Animator))]
+[RequireComponent (typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour
+{
+	public float speed = 6.0f;
+	public LayerMask floorMask;
+
+	private Animator _anim;
+	private Rigidbody _rg;
+	private float _camRayLength = 100.0f;
+
+	void Awake ()
+	{
+		_anim = GetComponent<Animator> ();
+		_rg = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate()
+	{
+		float h = Input.GetAxisRaw ("Horizontal");
+		float v = Input.GetAxisRaw ("Vertical");
+
+		Move (h, v);
+		Rotate ();
+		Animating (h, v);
+	}
+
+	private void Move(float h, float v)
+	{
+		Vector3 movement = new Vector3 (h, 0.0f, v);
+		movement = movement.normalized * speed * Time.deltaTime;
+		_rg.MovePosition (transform.position + movement);
+	}
+
+	private void Rotate()
+	{
+		Ray camRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+		RaycastHit floorHit;
+
+		if (Physics.Raycast(camRay, out floorHit, _camRayLength, floorMask))
+		{
+			Vector3 playerToMouse = floorHit.point - transform.position;
+			//playerToMouse.y = 0f;
+
+			Quaternion newRotation = Quaternion.LookRotation (playerToMouse);
+			_rg.MoveRotation (newRotation);
+		}
+	}
+
+	private void Animating (float h, float v)
+	{
+		bool walking = ( h != 0 || v != 0 );
+		_anim.SetBool ("IsWalking", walking);
+	}
+}
